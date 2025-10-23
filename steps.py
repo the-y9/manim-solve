085ffeea8d867ba27bda817216887fd4883bb82a
Yash Mishra <ymishra975@gmail.com>
2025-10-23 22:09:02 +0530

    Limit Problem | lim (n→∞) (1ᵏ + 2ᵏ + ... + nᵏ) / nᵏ⁺¹

--- steps.py
+++ steps.py
@@ -1,6 +1,6 @@
 from manim.utils.color import *
 
-TITLE_FONT_SIZE = 60
+TITLE_FONT_SIZE = 54
 STEP_FONT_SIZE = 48
 
 BGCOL = BLACK
@@ -8,32 +8,23 @@ qcolor = YELLOW
 
 question = r"""
 \begin{array}{c}
-u+v=8; u,v>0
+\lim_{n \to \infty} \dfrac{1^k + 2^k + 3^k + \cdots + n^k}{n^{k+1}} = \ ?
 \\
-A = 5u^2 + 11v^2
 \\
-min(A) = ?
+\text{if, } k \ge 1
 \end{array}
 """
+
 steps = [
-    (r"u+v = 8, A = 5u^2 + 11v^2", YELLOW),
-    (r"v = 8-u \Rightarrow A = 5u^2 + 11(8-u)^2", WHITE),
-    (r"\Rightarrow A = 5u^2 + 11(u^2-16u+64)", WHITE),
-    (r"\Rightarrow A = 5u^2 + 11u^2-176u+704", WHITE),
-    (r"\Rightarrow A = 16u^2 -176u +704", WHITE),
-
-    (r"\text{Differentiation Trick to find min: }", YELLOW),
-    (r"\frac{dA}{du} = 32u -176 = 0", WHITE),
-    (r"\Rightarrow u = \frac{176}{32} = 5.5", WHITE), 
-    (r"\Rightarrow v = 8 - u = 2.5", WHITE),
-
-    (r"\text{Double Check with 2nd Derivative: }", YELLOW),
-    (r"\frac{d^2A}{du^2} = 32 > 0 \Rightarrow \text{Min at } u=5.5", WHITE),
-
-    (r"\text{Calculate Minimum of}\ A", YELLOW),
-    (r"A_{min} = 5(5.5)^2 + 11(2.5)^2", WHITE),
-    (r"= 5(30.25) + 11(6.25) = 151.25 + 68.75", WHITE),
-    (r"= 220", GREEN),
-
+    (r"\lim_{n \to \infty} \frac{1^k + 2^k + \cdots + n^k}{n^{k+1}}", YELLOW),
+    (r"\text{Recall the sum of powers: }", YELLOW),
+    (r"1^k + 2^k + \cdots + n^k", WHITE),
+    (r"\approx \frac{n^{k+1}}{k+1} + \frac{n^k}{2} + \dots", WHITE),
+    (r"\text{Divide both sides by } n^{k+1}", YELLOW),
+    (r"\Rightarrow \frac{1^k + 2^k + \cdots + n^k}{n^{k+1}}", WHITE),
+    (r"\approx \frac{1}{k+1} + \frac{1}{2n} + \dots", WHITE),
+    (r"\text{As } n \to \infty, \frac{1}{2n} \to 0", WHITE),
+    (r"\text{Hence, } = \frac{1}{k+1}", GREEN),
 ]
+
 problem = (question, qcolor)
